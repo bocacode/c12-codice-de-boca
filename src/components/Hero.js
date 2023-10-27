bbb33f51e2f02f186197e7559ae3cd234b71eb3e
Todd H. Albert <todd@bocacode.com>
2023-10-27 11:33:31 -0400

    Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <header className="hero">
+    <header className='hero'>
       <h1>Codice d' Boca</h1>
     </header>
   )
--- src/components/hero.js
+++
@@ -1,7 +0,0 @@
-export default function Hero() {
-  return (
-    <header className='hero'>
-      <h1>Codice d' Boca</h1>
-    </header>
-  )
-}
