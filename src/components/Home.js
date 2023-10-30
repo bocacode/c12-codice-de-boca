8bc763cf773e4531a7b2c3815b392c8b9bab10b4
Todd H. Albert <todd@bocacode.com>
2023-10-30 16:58:17 -0400

    auto-fetch with useEffect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function SingleMenuComponent(props) {
 	console.log('props ->', props)
@@ -13,28 +13,18 @@ function SingleMenuComponent(props) {
 export default function Home() {
 	const [menuItems, setMenuItems] = useState() // = [stateVar, setter]
 
-	// 1. fetch data - DONE
-	// 2. get array of objects - DONE
-
-	// 3. render each object
-	// 3.1 create a state variable - DONE
-	// 3.2 put inside state variable the array of objects - DONE
-	// 3.3 display each item in dom
-	// 3.3.1 Map over the array of objects
-	// 3.3.2 return each item so that it displays on the browser
-
-	const handleBtnClick = () => {
+	useEffect(() => {
 		fetch('https://codice-boca.web.app/menu')
 			.then(res => res.json()) // contacting the API
 			.then(data => setMenuItems(data)) // get clean data form res
 			.catch(x => console.error(x))
-	}
+	}, [])
 
 	return (
 		<section className='menu-items'>
 			{!menuItems
-					// if no menu items, show button
-					? <button onClick={() => handleBtnClick()}>get data</button>
+					// if no menu items, show Loading...
+					? <h3>Loading...</h3>
 					// otherwise show menu items
 					: menuItems.map(singleItem => {
 							return (
